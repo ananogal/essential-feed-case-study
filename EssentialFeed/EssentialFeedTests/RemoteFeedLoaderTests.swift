9b587bf172337d2c70a12c96f80d9802fcce22a9
Ana Nogal <ana.nogal@gmail.com>
2020-10-28 12:41:37 +0100

    Added more test assertions for different error codes

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -43,12 +43,13 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
 
-        var captureErrors = [RemoteFeedLoader.Error]()
-        sut.load{ captureErrors.append($0) }
-
-        client.complete(withStatusCode: 400)
-
-        XCTAssertEqual(captureErrors, [.invalidData])
+        let samples = [199, 201, 300, 400, 500]
+        samples.enumerated().forEach { index, code in
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load{ capturedErrors.append($0) }
+            client.complete(withStatusCode: code, at: index)
+            XCTAssertEqual(capturedErrors, [.invalidData])
+        }
     }
 
     //MARK: - Helpers
